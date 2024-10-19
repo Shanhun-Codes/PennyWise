4ba611fec1feeaa014c2044ca413972c3f91215e
Travis Shanhun <travisshanhun@Traviss-MacBook-Air.local>
2024-10-19 16:18:39 -0500

    added minor styling to so I can see what I'm doing, created JSON data, addbill functionality working, and search functionality working, bills currently sorted by amount

--- app/lib/app.js
+++ app/lib/app.js
@@ -7,39 +7,103 @@ pennyWiseApp.config([
       .when("/home", {
         templateUrl: "views/home.html",
       })
-      .when('/budgetInput', {
-        templateUrl:'views/budgetInput.html'
+      .when("/budgetInput", {
+        templateUrl: "views/budgetInput.html",
       })
       .otherwise({ redirectTo: "/home" });
   },
 ]);
 
 // STATE CONTROLLER
-pennyWiseApp.controller('StateController', function($scope) {
-  $scope.states = [
-    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
-    'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-    'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
-    'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
-    'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-  ];
+pennyWiseApp.controller("StateController", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.states = [
+      "Alabama",
+      "Alaska",
+      "Arizona",
+      "Arkansas",
+      "California",
+      "Colorado",
+      "Connecticut",
+      "Delaware",
+      "Florida",
+      "Georgia",
+      "Hawaii",
+      "Idaho",
+      "Illinois",
+      "Indiana",
+      "Iowa",
+      "Kansas",
+      "Kentucky",
+      "Louisiana",
+      "Maine",
+      "Maryland",
+      "Massachusetts",
+      "Michigan",
+      "Minnesota",
+      "Mississippi",
+      "Missouri",
+      "Montana",
+      "Nebraska",
+      "Nevada",
+      "New Hampshire",
+      "New Jersey",
+      "New Mexico",
+      "New York",
+      "North Carolina",
+      "North Dakota",
+      "Ohio",
+      "Oklahoma",
+      "Oregon",
+      "Pennsylvania",
+      "Rhode Island",
+      "South Carolina",
+      "South Dakota",
+      "Tennessee",
+      "Texas",
+      "Utah",
+      "Vermont",
+      "Virginia",
+      "Washington",
+      "West Virginia",
+      "Wisconsin",
+      "Wyoming",
+    ];
 
-  $scope.addIncome = function() {
-    console.log('Adding income for state:', $scope.selectedState);
-  };
-});
+    $scope.addIncome = function () {
+      console.log("Adding income for state:", $scope.selectedState);
+    };
 
-// BILL CONTROLLER 
-pennyWiseApp.controller('BillController', ['$scope', '$http', function($scope, $http){
-    // Get JSON Data 
-    $http.get('../../data/billData.json').then(function(response){
-        $scope.bills = response.data
-    })
+    $http.get("../../data/incomeData.json").then(function (response) {
+      $scope.income = response.data;
+      console.log($scope.income);
+    });
+  },
+]);
 
+// BILL CONTROLLER
+pennyWiseApp.controller("BillController", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    // Get JSON Data
+    $http.get("../../data/billData.json").then(function (response) {
+      $scope.bills = response.data;
+    });
+
+    // Add bill functionality
     $scope.addBill = function () {
-        $scope.bills.push({
-            name: $scope.newBill.name,
-            dollarAmount: parseInt(Math.ceil($scope.newBill.dollarAmount))
-        })
-    }
-}])
+      $scope.bills.push({
+        name: $scope.newBill.name,
+        dollarAmount: parseInt(Math.ceil($scope.newBill.dollarAmount)),
+      });
+      //   reset inputs back to an empty string so user does not have to backspace out old entry after sumbit
+      $scope.newBill = { name: "", dollarAmount: "" };
+    };
+
+    // remove listed bill functionality
+    
+  },
+]);
