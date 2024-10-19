e378f7e97a33592d268b390c87396b076c661140
Travis Shanhun <travisshanhun@Traviss-MacBook-Air.local>
2024-10-19 18:27:04 -0500

    was having trouble passing data from one page to another, for the sake of getting it to work and moving on as this is being built to mainly to practice with angualr I decided to use local sorage for now.

--- app/lib/app.js
+++ app/lib/app.js
@@ -2,7 +2,7 @@ let pennyWiseApp = angular.module("pennyWiseApp", ["ngRoute"]);
 
 pennyWiseApp.config([
   "$routeProvider",
-  function ($routeProvider) {
+  ($routeProvider) => {
     $routeProvider
       .when("/home", {
         templateUrl: "views/home.html",
@@ -14,25 +14,36 @@ pennyWiseApp.config([
   },
 ]);
 
-// STATE CONTROLLER
+// INCOME CONTROLLER
 pennyWiseApp.controller("IncomeController", [
   "$scope",
   "$http",
-  function ($scope, $http) {
-    $http.get("../../data/statesData.json").then( (response) => {
-        $scope.states = response.data
-    })
+  "$window",
+  "$location",
+  ($scope, $http, $window, $location) => {
+    $http.get("../../data/statesData.json").then((response) => {
+      $scope.states = response.data;
+    });
 
+    $scope.newIncome;
+    $scope.income = 0;
+    $scope.postTaxIncome = 0;
 
-    $scope.addIncome = function () {
-      console.log("Adding income for state:", $scope.selectedState);
-
+    $scope.addIncome = () => {
+      $scope.income = $scope.newIncome;
+      $scope.postTaxIncome = $scope.income - $scope.income * 0.22;
+      // Store in local storage
+      $window.localStorage.setItem("postTaxIncome", JSON.stringify($scope.postTaxIncome));      
+      $location.path("/bills")
     };
 
-    $http.get("../../data/incomeData.json").then(function (response) {
-      $scope.income = response.data;
-      console.log($scope.income);
+
+    $scope.$watch("newIncome", function (newValue, oldValue) {
+      if (newValue !== oldValue) {
+        $scope.postTaxIncome = newValue - newValue * 0.22;
+      }
     });
+
   },
 ]);
 
@@ -40,14 +51,18 @@ pennyWiseApp.controller("IncomeController", [
 pennyWiseApp.controller("BillController", [
   "$scope",
   "$http",
-  function ($scope, $http) {
+  "$window",
+  ($scope, $http, $window) => {
+
+    $scope.postTaxIncome = JSON.parse($window.localStorage.getItem("postTaxIncome")) || 0;
+
     // Get JSON Data
-    $http.get("../../data/billData.json").then(function (response) {
+    $http.get("../../data/billData.json").then((response) => {
       $scope.bills = response.data;
     });
 
     // Add bill functionality
-    $scope.addBill = function () {
+    $scope.addBill = () => {
       $scope.bills.push({
         name: $scope.newBill.name,
         dollarAmount: parseInt(Math.ceil($scope.newBill.dollarAmount)),
@@ -58,13 +73,18 @@ pennyWiseApp.controller("BillController", [
 
     // remove listed bill functionality
     $scope.removeBill = (bill) => {
-        let removedBill = $scope.bills.indexOf(bill)
-        console.log(removedBill)
-        $scope.bills.splice(removedBill, 1)
-    }
+      let removedBill = $scope.bills.indexOf(bill);
+      console.log(removedBill);
+      $scope.bills.splice(removedBill, 1);
+    };
+
+    $scope.$watch(function() {
+        return $window.localStorage.getItem('postTaxIncome');
+    }, function(newValue) {
+        $scope.postTaxIncome = parseFloat(newValue) || 0;
+        // Recalculate percentages or update view as needed
+    });
   },
 ]);
 
-pennyWiseApp.controller("BudgetController"['$scope', ($scope) => {
-
-}])
+pennyWiseApp.controller("BudgetController"[("$scope", ($scope) => {})]);
