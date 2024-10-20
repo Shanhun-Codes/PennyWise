cb633b5d935e638e82ce2fee55c10a8964cbe242
Travis Shanhun <travisshanhun@Traviss-MacBook-Air.local>
2024-10-20 08:04:49 -0500

    finished finished functionality on bills page, used localStorage to store total amount of bills

--- app/lib/app.js
+++ app/lib/app.js
@@ -11,7 +11,7 @@ pennyWiseApp.config([
         templateUrl: "views/billsInput.html",
       })
       .when("/budget", {
-        templateUrl: "views/budget.html"
+        templateUrl: "views/budget.html",
       })
       .otherwise({ redirectTo: "/home" });
   },
@@ -36,17 +36,18 @@ pennyWiseApp.controller("IncomeController", [
       $scope.income = $scope.newIncome;
       $scope.postTaxIncome = $scope.income - $scope.income * 0.22;
       // Store in local storage
-      $window.localStorage.setItem("postTaxIncome", JSON.stringify($scope.postTaxIncome));      
-      $location.path("/bills")
+      $window.localStorage.setItem(
+        "postTaxIncome",
+        JSON.stringify($scope.postTaxIncome)
+      );
+      $location.path("/bills");
     };
-
 
     $scope.$watch("newIncome", function (newValue, oldValue) {
       if (newValue !== oldValue) {
         $scope.postTaxIncome = newValue - newValue * 0.22;
       }
     });
-
   },
 ]);
 
@@ -55,9 +56,10 @@ pennyWiseApp.controller("BillController", [
   "$scope",
   "$http",
   "$window",
-  ($scope, $http, $window) => {
-
-    $scope.postTaxIncome = JSON.parse($window.localStorage.getItem("postTaxIncome")) || 0;
+  "$location",
+  ($scope, $http, $window, $location) => {
+    $scope.postTaxIncome =
+      JSON.parse($window.localStorage.getItem("postTaxIncome")) || 0;
 
     // Get JSON Data
     $http.get("../../data/billData.json").then((response) => {
@@ -66,24 +68,31 @@ pennyWiseApp.controller("BillController", [
 
     // Add bill functionality
     $scope.addBill = () => {
-        // Check if both fields are filled
-        if (!$scope.newBill.name || !$scope.newBill.dollarAmount) {
-            $scope.errorMessage = "Both fields are required!";
-            return; // Exit the function early
-        } else $scope.errorMessage = ""
-    
-        // Clear any previous error message
-        $scope.formError = "";
-    
-        // Add the bill
-        $scope.bills.push({
-            name: $scope.newBill.name,
-            dollarAmount: parseInt(Math.ceil($scope.newBill.dollarAmount)),
-        });
-    
-        // Reset inputs
-        $scope.newBill = { name: "", dollarAmount: "" };
-    
+      // Check if both fields are filled
+      if (!$scope.newBill.name || !$scope.newBill.dollarAmount) {
+        $scope.errorMessage = "Both fields are required!";
+        return; // Exit the function early
+      } else $scope.errorMessage = "";
+
+      // Clear any previous error message
+      $scope.formError = "";
+
+      // calculate percentage
+      let billAmount = parseInt(Math.ceil($scope.newBill.dollarAmount));
+      let billPercentage = (
+        (billAmount / ($scope.postTaxIncome / 12)) *
+        100
+      ).toFixed(2);
+
+      // Add the bill
+      $scope.bills.push({
+        name: $scope.newBill.name,
+        dollarAmount: billAmount,
+        // percentage: billPercentage,
+      });
+
+      // Reset inputs
+      $scope.newBill = { name: "", dollarAmount: "" };
     };
 
     // remove listed bill functionality
@@ -93,13 +102,29 @@ pennyWiseApp.controller("BillController", [
       $scope.bills.splice(removedBill, 1);
     };
 
-    $scope.$watch(function() {
-        return $window.localStorage.getItem('postTaxIncome');
-    }, function(newValue) {
+    $scope.$watch(
+      function () {
+        return $window.localStorage.getItem("postTaxIncome");
+      },
+      function (newValue) {
         $scope.postTaxIncome = parseFloat(newValue) || 0;
         // Recalculate percentages or update view as needed
-    });
+      }
+    );
+
+    // finished btn functionality need to grab totals of all bill items and either store total in local storage or indivual key and totals. Easier and better for device storage just to store total
+    $scope.finished = () => {
+        let totalAmountOfBills = 0
+        $scope.bills.map((bill) => {
+            totalAmountOfBills += bill.dollarAmount
+        })
+        console.log(totalAmountOfBills)
+        $window.localStorage.setItem('totalAmountOfBills', JSON.stringify(totalAmountOfBills))
+        $location.path('/budget')
+    }
   },
 ]);
 
-pennyWiseApp.controller("BudgetController"[("$scope", ($scope) => {})]);
+pennyWiseApp.controller(
+  "BudgetController"[("$scope", "$window", ($scope, $window) => {})]
+);
